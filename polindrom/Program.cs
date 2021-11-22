a0c6a813ad8b8bc01535efb95851ac0fa68f920c
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-22 13:11:44 +0500

    Добавьте файлы проекта.

---
+++ polindrom/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string resultstr = str.ToLower();
+            resultstr = resultstr.Replace(" ", string.Empty);
+            string resultstr1 = "";
+            for (int i = resultstr.Length - 1; i >= 0; --i)
+
+            {
+                resultstr1 += (resultstr[i]);
+            }
+            if (resultstr1 == resultstr)
+            {
+                Console.WriteLine("Предложение-полиндром");
+            }
+            else
+                Console.WriteLine("Предложение - не полиндром");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ string/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strings = str.Split();
+            string count = "";
+            string searchString = "0";
+            foreach (string s in strings)
+            {
+                count += s.Length;
+                if (s.Length > searchString.Length)
+                {
+                    searchString = s;
+                }
+
+
+
+            }
+
+
+            Console.WriteLine("Самое длинное слово в строке {0}", searchString);
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
